69f660bfa020f3a2510513599abdb2f4125dba77
ivanh <rik.chanel@gmail.com>
2022-10-11 22:55:27 +0300

    start write game logic, init classes and functions

---
+++ app/src/main/java/com/example/lesson6_twopars/Board.java
@@ -0,0 +1,8 @@
+package com.example.lesson6_twopars;
+
+public class Board {
+    private int stepsCount;
+    private int stepsFinish;
+    private boolean winOrLose;
+    private Couples[] openCouples;
+}
---
+++ app/src/main/java/com/example/lesson6_twopars/Couples.java
@@ -0,0 +1,27 @@
+package com.example.lesson6_twopars;
+
+public class Couples {
+    private int firstCard; // id picture (getdraweble)
+    private int secondCard; // id picture
+    private boolean equal;
+
+    public int getFirstCard() {
+        return firstCard;
+    }
+
+    public void setFirstCard(int firstCard) {
+        this.firstCard = firstCard;
+    }
+
+    public int getSecondCard() {
+        return secondCard;
+    }
+
+    public void setSecondCard(int secondCard) {
+        this.secondCard = secondCard;
+    }
+
+    public boolean isEqual() {
+        return firstCard == secondCard? true : false;
+    }
+}
