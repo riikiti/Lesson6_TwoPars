189906b8b1198244f54b225a470e4e20538e03b2
ivanh <rik.chanel@gmail.com>
2022-10-25 23:02:44 +0300

    idk

--- app/src/main/java/com/example/lesson6_twopars/MainActivity.java
+++ app/src/main/java/com/example/lesson6_twopars/MainActivity.java
@@ -5,29 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    Board board = new Board();
-    ImageView card0 = (ImageView) findViewById(R.id.imageView1);
-    ImageView card1 = (ImageView) findViewById(R.id.imageView2);
-    ImageView card2 = (ImageView) findViewById(R.id.imageView3);
-    ImageView card3 = (ImageView) findViewById(R.id.imageView4);
-    ImageView card4 = (ImageView) findViewById(R.id.imageView5);
-    ImageView card5 = (ImageView) findViewById(R.id.imageView6);
-    ImageView card6 = (ImageView) findViewById(R.id.imageView7);
-    ImageView card7 = (ImageView) findViewById(R.id.imageView8);
-    ImageView card8 = (ImageView) findViewById(R.id.imageView9);
-    ImageView card9 = (ImageView) findViewById(R.id.imageView10);
-    ImageView card10 = (ImageView) findViewById(R.id.imageView11);
-    ImageView card11 = (ImageView) findViewById(R.id.imageView12);
+    public Board board ;
+    public ImageView card0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        board=new Board();
+        card0 = (ImageView) findViewById(R.id.imageView1);
+        card1 = (ImageView) findViewById(R.id.imageView2);
+        card2 = (ImageView) findViewById(R.id.imageView3);
+        card3 = (ImageView) findViewById(R.id.imageView4);
+        card4 = (ImageView) findViewById(R.id.imageView5);
+        card5 = (ImageView) findViewById(R.id.imageView6);
+        card6 = (ImageView) findViewById(R.id.imageView7);
+        card7 = (ImageView) findViewById(R.id.imageView8);
+        card8 = (ImageView) findViewById(R.id.imageView9);
+        card9 = (ImageView) findViewById(R.id.imageView10);
+        card10 = (ImageView) findViewById(R.id.imageView11);
+        card11 = (ImageView) findViewById(R.id.imageView12);
 
         card0.setOnClickListener((View.OnClickListener) this);
         card1.setOnClickListener((View.OnClickListener) this);
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         card9.setOnClickListener((View.OnClickListener) this);
         card10.setOnClickListener((View.OnClickListener) this);
         card11.setOnClickListener((View.OnClickListener) this);
+
     }
 
     public void Check(View view) {
@@ -99,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public Drawable GetImg(int[][] board, int i, int j) {
-        Drawable drawableCard =getDrawable(R.drawable.card);
+        Drawable drawableCard = getDrawable(R.drawable.card);
         switch (board[i][j]) {
             case 0:
                 drawableCard = getDrawable(R.drawable.ace);
